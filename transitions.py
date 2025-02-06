c0028f4f4f217d47ea6c23319144210eb7561cff
Girish Kalyan Jalasutram <54175783+Girish16@users.noreply.github.com>
2025-02-05 23:13:42 -0800

    Create transitions

---
+++ transitions.py
@@ -0,0 +1,20 @@
+class ReplayBuffer:
+    def __init__(self, max_size):
+        self.buffer = deque(maxlen=max_size)
+    
+    def add(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+    
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        states, actions, rewards, next_states, dones = zip(*batch)
+        return (
+            np.array(states, dtype=np.float32),
+            np.array(actions, dtype=np.int64),
+            np.array(rewards, dtype=np.float32),
+            np.array(next_states, dtype=np.float32),
+            np.array(dones, dtype=np.float32)
+        )
+    
+    def size(self):
+        return len(self.buffer)

18bfb28535002d67253da4713547690a93cdcbab
Girish Kalyan Jalasutram <54175783+Girish16@users.noreply.github.com>
2025-02-05 23:09:06 -0800

    Create libraries

---
+++ libraries.py
@@ -0,0 +1,6 @@
+
+pip install gymnasium[atari] # For Atari environments
+
+pip install tensorflow tensorflow-gpu  # If you prefer TensorFlow/Keras
+
+pip install numpy opencv-python  # CV2 might be needed for observation preprocessing
